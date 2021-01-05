9343ad28cf9cdc4db296d0d54204a27e22ea09ba
Aayush Atharva <aayush@shieldblaze.com>
2021-01-05 16:52:49 +0530

    Shift to Apache License 2.0 and add macOS Support (#3)

--- brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
+++ brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
@@ -1,19 +1,18 @@
 /*
- * This file is part of Brotli4j.
- * Copyright (c) 2020 Aayush Atharva
+ *    Copyright 2021, Aayush Atharva
  *
- * Brotli4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    Brotli4j licenses this file to you under the
+ *    Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * Brotli4j is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with Brotli4j.  If not, see <https://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package com.aayushatharva.brotli4j;
 
@@ -94,6 +93,10 @@ public class Brotli4jLoader {
         } else if (osName.startsWith("Windows")) {
             if (archName.equalsIgnoreCase("amd64")) {
                 return "windows_x86-64";
+            }
+        } else if (osName.startsWith("Mac")) {
+            if (archName.equalsIgnoreCase("x86_64")) {
+                return "macos_x86-64";
             }
         }
         throw new UnsupportedOperationException("Unsupported OS and Architecture: " + osName + ", " + archName);
---
+++ natives/macos_x86-64/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CURPATH=$(pwd)
+TARGET_CLASSES_PATH="target/classes/lib/macos_x86-64"
+TARGET_PATH="target"
+
+function exitWithError() {
+  cd ${CURPATH}
+  echo "*** An error occurred. Please check log messages. ***"
+  exit $1
+}
+
+mkdir -p "$TARGET_CLASSES_PATH"
+
+cd "$TARGET_PATH"
+cmake ../../../ || exitWithError $?
+make || exitWithError $?
+rm -f "$CURPATH/${TARGET_CLASSES_PATH}/libbrotli.dylib"
+cp "./libbrotli.dylib" "$CURPATH/${TARGET_CLASSES_PATH}" || exitWithError $?
+
+cd "${CURPATH}"
