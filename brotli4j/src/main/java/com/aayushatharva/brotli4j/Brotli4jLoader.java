716c5f78c5a6f28ec32b186fb739931f522143d3
Aayush Atharva <aayush@shieldblaze.com>
2021-01-05 10:54:07 +0530

    macOS

--- brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
+++ brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
@@ -100,6 +100,8 @@ public class Brotli4jLoader {
     private static String getPlatform() {
         String osName = System.getProperty("os.name");
         String archName = System.getProperty("os.arch");
+        System.out.println(osName);
+        System.out.println(archName);
         if (osName.equalsIgnoreCase("Linux")) {
             if (archName.equalsIgnoreCase("amd64")) {
                 return "linux_x86-64";
