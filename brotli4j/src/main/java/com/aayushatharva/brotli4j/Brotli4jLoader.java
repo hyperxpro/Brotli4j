9fd11923a91e80ebd48b68045824cde74eb03f07
Aayush Atharva <aayush@shieldblaze.com>
2021-01-05 11:51:35 +0530

    macOS

--- brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
+++ brotli4j/src/main/java/com/aayushatharva/brotli4j/Brotli4jLoader.java
@@ -1,19 +1,18 @@
 /*
- * This file is part of Brotli4j.
- * Copyright (c) 2020 Aayush Atharva
+ *    Copyright 2021, Aayush Atharva
  *
- * Brotli4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    Brotli4j licenses this file to you under the
+ *    Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * Brotli4j is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with Brotli4j.  If not, see <https://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package com.aayushatharva.brotli4j;
 
@@ -38,8 +37,6 @@ public class Brotli4jLoader {
                 String nativeLibName = System.mapLibraryName("brotli");
                 String libPath = "/lib/" + getPlatform() + "/" + nativeLibName;
 
-                listOfFiles(new File("/Users/runner/work/Brotli4j/Brotli4j/"));
-
                 File tempDir = new File(System.getProperty("java.io.tmpdir"), "com_aayushatharva_brotli4j_" + System.nanoTime());
                 tempDir.mkdir();
                 tempDir.deleteOnExit();
@@ -62,17 +59,6 @@ public class Brotli4jLoader {
         }
 
         UNAVAILABILITY_CAUSE = cause;
-    }
-
-    public static void listOfFiles(File dirPath){
-        File filesList[] = dirPath.listFiles();
-        for(File file : filesList) {
-            if(file.isFile()) {
-                System.out.println("File path: "+file.getAbsolutePath());
-            } else {
-                listOfFiles(file);
-            }
-        }
     }
 
     /**
