b6604136cf1f0a450c39afcab4bab3cb10fa17a4
Aayush Atharva <aayush@shieldblaze.com>
2021-01-05 09:59:52 +0530

    macOS

--- natives/macos_x86-64/build.sh
+++ natives/macos_x86-64/build.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 CURPATH=$(pwd)
 TARGET_CLASSES_PATH="target/classes/lib/macos_x86-64"
 TARGET_PATH="target"
 
-function exitWithError() {
+exitWithError() {
   cd ${CURPATH}
   echo "*** An error occurred. Please check log messages. ***"
   exit $1
