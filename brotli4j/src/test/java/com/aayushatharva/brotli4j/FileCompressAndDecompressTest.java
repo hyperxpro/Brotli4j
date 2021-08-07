c23bd45f6a3a5bf72d4daa1d80c5868941b670d7
Aayush Atharva <aayush@shieldblaze.com>
2021-08-07 20:52:58 +0530

    Fix resource leak

--- brotli4j/src/test/java/com/aayushatharva/brotli4j/FileCompressAndDecompressTest.java
+++ brotli4j/src/test/java/com/aayushatharva/brotli4j/FileCompressAndDecompressTest.java
@@ -54,11 +54,12 @@ class FileCompressAndDecompressTest {
     }
 
     static byte[] getBytes(InputStream is) throws IOException {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] buffer = new byte[0xFFFF];
-        for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
-            os.write(buffer, 0, len);
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[0xFFFF];
+            for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
+                os.write(buffer, 0, len);
+            }
+            return os.toByteArray();
         }
-        return os.toByteArray();
     }
 }
