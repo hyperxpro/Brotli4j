0ec1cce34531725432c9149c7f0165df966e8125
Jarek Strzelecki <jarst@users.noreply.github.com>
2022-10-11 15:12:09 +0200

    Remove GPL license text for sources with MIT license

--- brotli4j/src/main/java/com/aayushatharva/brotli4j/encoder/BrotliOutputStream.java
+++ brotli4j/src/main/java/com/aayushatharva/brotli4j/encoder/BrotliOutputStream.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of Brotli4j.
- * Copyright (c) 2020-2022 Aayush Atharva
- *
- * Brotli4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Brotli4j is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Brotli4j.  If not, see <https://www.gnu.org/licenses/>.
- */
 /* Copyright 2017 Google Inc. All Rights Reserved.
 
    Distributed under MIT license.
